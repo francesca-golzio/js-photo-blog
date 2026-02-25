d310ca8e5b1d95913ab2d12bb16050e41dc9397f
Francesca Golzio <francescagolzio@gmail.com>
2026-02-25 12:37:28 +0100

    fetch endpoint

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+/* is this working? */
+console.log('🍏');
+
+
+/* Collect datas */
+const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
+const rowEl = document.querySelector('row');
+
+
+/* AJAX call to API */
+
+fetch(endpoint)
+  .then(res => res.json())
+  .then(photos => {
+    console.log(photos);
+
+    /* Cycling inside the array */
+   /*  photos.forEach(photo => { */
+      
+      /* Destructuring object */
+      /* const {id, title, date, url} = photo;
+      //console.log(id, title, date, url); */
+      
+      /* Defining markdown */
+      /* const markdown = `
+      <div class="photo col d-flex flex-column justify-content-center">
+        <img src="${url}" alt="${title}" id="${id}">
+        <h2 id="titoloPhoto">
+          ${title}
+        </h2>
+        <span id="dataPhoto">
+          ${date}
+        </span>
+        </div>
+      `;
+      console.log(markdown); */
+      
+      
+   /*  }); */
+    
+  })
+
+
+    /* Printing object datas in page */
+
